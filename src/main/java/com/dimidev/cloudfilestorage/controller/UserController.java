abfc43611d43357dfcdbeae4fd164843214f5728
ChestnutLord <134998927+ChestnutLord@users.noreply.github.com>
2026-07-13 12:17:35 +0300

    feat(user): add GET /user/me endpoint

--- src/main/java/com/dimidev/cloudfilestorage/controller/UserController.java
+++ src/main/java/com/dimidev/cloudfilestorage/controller/UserController.java
@@ -1,6 +1,20 @@
 package com.dimidev.cloudfilestorage.controller;
 
 import com.dimidev.cloudfilestorage.controller.api.UserApi;
+import com.dimidev.cloudfilestorage.dto.user.UserReadDto;
+import com.dimidev.cloudfilestorage.security.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/user")
 public class UserController implements UserApi {
+
+    @Override
+    @GetMapping("/me")
+    public UserReadDto me(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return new UserReadDto(userDetails.getUsername());
+    }
 }
--- src/main/java/com/dimidev/cloudfilestorage/controller/api/UserApi.java
+++ src/main/java/com/dimidev/cloudfilestorage/controller/api/UserApi.java
@@ -1,4 +1,20 @@
 package com.dimidev.cloudfilestorage.controller.api;
 
+import com.dimidev.cloudfilestorage.dto.user.UserReadDto;
+import com.dimidev.cloudfilestorage.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Tag(name = "User", description = "Информация о текущем пользователе")
 public interface UserApi {
+
+    @Operation(summary = "Получение текущего пользователя")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Пользователь получен"),
+            @ApiResponse(responseCode = "401", description = "Неавторизован")
+    })
+    UserReadDto me(@AuthenticationPrincipal CustomUserDetails userDetails);
 }
